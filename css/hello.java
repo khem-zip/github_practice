6e2754a345c2b2a3cff8c1ddf672bf322a469a97
Khem Raj Bajgain <112122589+khem-zip@users.noreply.github.com>
2024-04-02 12:16:13 +0530

    Update hello.java for seeing the change
    added the new concept here for practice

--- css/hello.java
+++ css/hello.java
@@ -1,5 +1,10 @@
 class hello {
     public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int averageSum = a + b;
+        
         System.out.println("Hello World");
+        System.out.println("Average sum is: " averageSum);
     }
 }
